a5c8db693ae7e47c828fb622fbedb779b56ea855
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-12 01:20:07 +0200

    Podstawa funkcjonuje

--- request-update/src/main/java/com/novelCheck/requestupdate/resources/RequestUpdate.java
+++ request-update/src/main/java/com/novelCheck/requestupdate/resources/RequestUpdate.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.stream.Collectors;
 @RestController
@@ -18,21 +18,12 @@ public class RequestUpdate {
 
     private RestTemplate restTemplate = new RestTemplate();
     @RequestMapping("/{userID}")
-    /*public String shid(@PathVariable("userID") String userID){
-        return "rip "+ userID;
-    }*/
     public List<KatalogNovel> getKatalog(@PathVariable("userID") String userID){
-        //return (List<KatalogNovel>) new KatalogNovel("2","asds");
-        UserKatalog userKatalog = restTemplate.getForObject("http://localhost:8082/update/users/a",UserKatalog.class);//w inf zwrotnej mam obiekt UserKatalog class
+
+        UserKatalog userKatalog = restTemplate.getForObject("http://localhost:8082/update/users/"+userID,UserKatalog.class);//w inf zwrotnej mam obiekt UserKatalog class
        KatalogUpdate as = new KatalogUpdate();
 
-        /*
-        List<KatalogUpdate> katalog = Arrays.asList(
-                new KatalogUpdate("the-second-coming-of-gluttony"),
-                new KatalogUpdate("second-life-ranker")
-        );*/
-        //katalog
-        String url ;//"second-life-ranker";
+
         return userKatalog.getNovels().stream().map(novelka -> {//problem w konstukcji url//Już działa
             NovelChap novelChap = restTemplate.getForObject( "http://localhost:8080/scrape/" +novelka.getNovelID(),NovelChap.class);
             return new KatalogNovel( novelChap.getChapNum() , novelChap.getChapLink());//novelka.getNovelID()
