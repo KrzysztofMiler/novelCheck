f89685d9b83f8634612d8f9cb685a704ce06f43d
Krzysztof Miler <krzy.miler@gmail.com>
2020-09-26 17:11:05 +0200

    Get dla wszystkich userów i pojedeyńczych userów

--- update-dbH2/src/main/java/com/novelCheck/updatedbH2/repository/UserUserRepo.java
+++ update-dbH2/src/main/java/com/novelCheck/updatedbH2/repository/UserUserRepo.java
@@ -3,7 +3,12 @@ package com.novelCheck.updatedbH2.repository;
 import com.novelCheck.updatedbH2.model.UserUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserUserRepo extends CrudRepository<UserUser,Long> {
+    List<UserUser> findByUserName(String username);
+
+    //List<UserUser> findByUserName (String userName);
 
 
 
--- update-dbH2/src/main/java/com/novelCheck/updatedbH2/resources/UpdateDBh2.java
+++ update-dbH2/src/main/java/com/novelCheck/updatedbH2/resources/UpdateDBh2.java
@@ -3,42 +3,62 @@ package com.novelCheck.updatedbH2.resources;
 
 import com.novelCheck.updatedbH2.model.KatalogUpdate;
 import com.novelCheck.updatedbH2.model.UserKatalog;
+import com.novelCheck.updatedbH2.model.UserUser;
 import com.novelCheck.updatedbH2.repository.KatalogUpdateRepo;
+import com.novelCheck.updatedbH2.repository.UserUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-
+//TODO zobaczyć czy @Service ma sens
 @RestController
 @RequestMapping("/db")
 public class UpdateDBh2 {
 
         @Autowired
-        private KatalogUpdateRepo repository;
+        private KatalogUpdateRepo katalogUpdateRepo;
+
+        @Autowired
+        private UserUserRepo userUserRepo;
 
         @GetMapping("/getAll")
         public UserKatalog getAll(){
             //repository.findAll();
             UserKatalog userKatalog = new UserKatalog();
-            userKatalog.setNovels((List<KatalogUpdate>)repository.findAll());
+            userKatalog.setNovels((List<KatalogUpdate>) katalogUpdateRepo.findAll());
             return userKatalog;
         }
         @PostMapping(value = "/saveNovel",consumes = "application/json",produces = "application/json")
         public String saveNovel(@RequestBody KatalogUpdate katalogUpdate){
-            repository.save(katalogUpdate);
+            katalogUpdateRepo.save(katalogUpdate);
             return "zaposano";//do testu
         }
         @GetMapping("/getNovel/{novelID}")
         public UserKatalog getNovel(@PathVariable("novelID") String novelID){
             UserKatalog userKatalog = new UserKatalog();
-            userKatalog.setNovels(repository.findByNovelID(novelID));//tutaj nie można zcastować (List<KatalogUpdate>)
+            userKatalog.setNovels(katalogUpdateRepo.findByNovelID(novelID));//tutaj nie można zcastować (List<KatalogUpdate>)
             return userKatalog;
         }
-    @GetMapping("/getStrona/{strona}")
+        @GetMapping("/getStrona/{strona}")
         public UserKatalog getStrona(@PathVariable("strona") String strona){
-        UserKatalog userKatalog = new UserKatalog();//działa ale głupio
-        userKatalog.setNovels(repository.findByStrona(strona));
-        return userKatalog;
-    }
+            UserKatalog userKatalog = new UserKatalog();//działa ale głupio
+            userKatalog.setNovels(katalogUpdateRepo.findByStrona(strona));
+            return userKatalog;
+        }
+
+        @GetMapping("/getAllUsers")
+        public List<UserUser> getAllUsers(){
+            List<UserUser> userUser = new ArrayList<UserUser>();
+            userUserRepo.findAll().forEach(user -> userUser.add(user));//trochę bardziej mi się podoba
+            return userUser;//działa ale zwraca lisę obj może będą z tym poblemy
+        }
+        @GetMapping("/getUser/{username}")
+        public List<UserUser> getUser(@PathVariable("username") String username){
+            List<UserUser> userUser = new ArrayList<UserUser>();
+            userUserRepo.findByUserName(username).forEach(user -> userUser.add(user));
+            return userUser;
+        }
+
 }
 
