79557b414a66549f40c69b2bc064a859309d5229
Krzysztof Miler <krzy.miler@gmail.com>
2020-10-14 22:13:36 +0200

    początek integracji z msg z messenger4j

---
+++ Messanger/src/test/java/com/novelCheck/Messanger/MessangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelCheck.Messanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
